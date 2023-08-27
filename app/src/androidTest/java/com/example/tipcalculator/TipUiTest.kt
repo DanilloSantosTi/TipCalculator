0d977c0b3e597813b9352717112ecd9a581d0f05
DanilloSantosTi <danillomdsti@gmail.com>
2023-08-27 20:08:05 -0300

    [master] feature - create project

---
+++ app/src/androidTest/java/com/example/tipcalculator/TipUiTest.kt
@@ -0,0 +1,31 @@
+package com.example.tipcalculator
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.example.tiptime.ui.theme.TipTimeTheme
+import java.text.NumberFormat
+import org.junit.Rule
+import org.junit.Test
+
+class TipUiTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_rule() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeLayout()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip")
+            .assertExists("No node with this text was found.")
+    }
+}
---
+++ app/src/test/java/com/example/tipcalculator/TipCalculatorTests.kt
@@ -0,0 +1,17 @@
+package com.example.tipcalculator
+
+import java.text.NumberFormat
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TipCalculatorTests {
+
+    @Test
+    fun calculateTip_20PercentNoRoundUp() {
+        val amount = 10.00
+        val tipPercent = 20.00
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        val actualTip = calculateTip(amount = amount, tipPercent = tipPercent, false)
+        assertEquals(expectedTip, actualTip)
+    }
+}
